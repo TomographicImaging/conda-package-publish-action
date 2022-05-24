2d22d640bec5f6b90f37fb37695fcb2044890a9f
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2022-05-24 14:15:22 +0100

    remove prints

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,7 @@ set -ex
 set -o pipefail
 
 setup_docker(){
-    echo ${INPUT_ADDITIONAL_APT_PACKAGES}
     if [ ! -z "${INPUT_ADDITIONAL_APT_PACKAGES}" ]; then
-        echo ${INPUT_ADDITIONAL_APT_PACKAGES}
         apt-get install -y  ${INPUT_ADDITIONAL_APT_PACKAGES}
     fi
 }
